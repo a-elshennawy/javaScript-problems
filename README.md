16049913e02e3399b7ad1c23a781b94cfe21aaf5
shennawy <ahmedshennawy997@gmail.com>
2025-10-07 15:57:33 +0300

    Big-O classic

--- README.md
+++ README.md
@@ -5,7 +5,17 @@
 <li>string manipulation</li>
 <li>Object & Data Structure Challenges</li>
 <li>Function Implementation</li>
-<li>DOM & Practical Challenges (you will need to use HTML for this) </li> 
+<li>DOM & Practical Challenges (you will need to use HTML livserver for this) </li> 
 </ul>
 <hr/>
-<h5>note: just do "<script src="neededFile.js"></script>"</h5>
+<h5>notes: just do "<script src="neededFile.js"></script>" <br/> after each challenge you will also get what's the methods Time / Space Complexity</h5>
+<h5>Time Complexity: measures how long your code takes to run as the input size grows. It's about speed. <br/>
+ O(1) - constant: same time regardless of input size (accessing an array element) <br/>
+ O(n) - linear: time grows with input size (looping through an array) <br/>
+ O(n²) - quadratic: nested loops, gets slow quickly
+</h5>
+<h5>Space Complexity: measures how much memory your code uses as input grows. It's about RAM. <br/>
+ Creating variables? That's space. <br/>
+ Copying arrays? That's more space. <br/>
+ Recursion? Each call uses stack space. <br/>
+</h5>
--- classicAlgorithms.js
+++ classicAlgorithms.js
@@ -1,4 +1,3 @@
-// coming will be most famous JS tech. interview questions (uncomment to test)
 // classic algorithm challenges
 
 //__________________________________________________________________________
@@ -16,6 +15,20 @@
 // const reversedString = reversed(string);
 // console.log(reversedString);
 
+// time:
+// Splitting the string with `split("")` takes O(n),
+// where n is the length of the string
+// Reversing the array with `reverse()` also takes O(n).
+// Joining the array back into a string with `join("")` takes O(n).
+// Overall, the total time complexity is O(n) + O(n) + O(n) = O(n).
+
+// space:
+// The `split` operation creates an array of size n, which requires O(n) space.
+// The `reverse` operation is in-place and does not require additional space.
+// The `join` operation creates a new string of length n, requiring O(n) space.
+// the total space complexity is dominated by the array and the resulting string,
+// which is O(n).
+
 // b: second way
 
 // function reverse(string) {
@@ -27,6 +40,14 @@
 // }
 // reverse(string);
 
+// time:
+// O(n), where n is the length of the input string.
+// because the loop runs n times
+
+// space:
+// O(n), since the reversedString variable stores a new string that in worset case
+// is the same length as the input string
+
 // c: third way
 // function reverse(string) {
 //   let reversedString = "";
@@ -37,15 +58,16 @@
 // }
 // reverse(string);
 
-// d: fourth way
-// function reversed(string) {
-//   return Array.from(string).reduce(
-//     (reversedString, c) => c + reversedString,
-//     ""
-//   );
-// }
-// const reversedString = reversed(string);
-// console.log(reversedString);
+// time:
+// iterates through each character of the input string exactly once,
+// starting from the last character to the first.
+// if length is n, the loop runs n times
+// overall time complexity is O(n).
+
+// space:
+// creates a new string reversedString that,
+// in the worst case, will be of length n.
+// the space complexity is O(n).
 
 //__________________________________________________________________________
 
@@ -57,6 +79,9 @@
 //   return string === string.split("").reverse().join("");
 // }
 // console.log(`palindrome is : ${checkPalindrome(textTocheck)}`);
+
+// time: O(n).
+// space: O(n).
 
 //__________________________________________________________________________
 
@@ -71,6 +96,9 @@
 //   console.log(min, max);
 // }
 // minMax(array);
+
+// time: O(n)
+// space: O(n)
 
 // b: no built-in (with addition of second largest number)
 // let min = array[0];
@@ -92,17 +120,16 @@
 // console.log("largest number is :", max);
 // console.log("second largest number is :", secMax);
 
+// time: O(n)
+// space: O(1)
+
 //__________________________________________________________________________
 
 // 6- Remove duplicates from an array
 // const nums = [1, 33, 44, 6, 1, 44];
 
-// a :
 // const noDups = [...new Set(nums)];
 // console.log(noDups);
 
-// b:
-// const noDups = nums.filter((n, i) => {
-//   return nums.indexOf(n) === i;
-// });
-// console.log(noDups);
+// time: O(n)
+// space: O(n)
